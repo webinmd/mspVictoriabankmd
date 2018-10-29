16da39d4d882e9a38799e7f84aa5ecb6be819208
Евгений <kuznetsov.evgheny@gmail.com>
2018-10-29 16:01:01 +0300

    Add bank tests

--- assets/components/minishop2/payment/victoriabankmd.php
+++ assets/components/minishop2/payment/victoriabankmd.php
@@ -38,19 +38,66 @@ if($_GET['checkoutUrl']){
 	/* @var msPaymentInterface|Victoriabankmd $handler */
 	$handler = new Victoriabankmd($modx->newObject('msOrder'));
 
-	if($_REQUEST['TRTYPE'] == 0 &&  $_REQUEST['TEXT']=='Approved'){		
-		$handler->payCompletition($_REQUEST);
+	$istest1 = false; // set false to disable test requests and true to enable
+	$istest2 = false; // set false to disable test requests and true to enable
+	$istest3 = false; // set false to disable test requests and true to enable
+
+	// start test requests 
+	// 1. pay(0) - completion(21)
+	// 2. pay(0) - reversal request(24)
+	// 3. pay(0) - completion(21) - reversal request(24)
+
+	// все данные (RRN транзакции) будут указаны в письме от банка
+
+
+	// 1
+	if($istest1){
+		if($_REQUEST['TRTYPE'] == 0 &&  $_REQUEST['TEXT']=='Approved'){		 
+			$handler->payCompletion($_REQUEST, 21); //trtype = 21
+		} 
 	}
 
-	if($_REQUEST['TRTYPE'] == 21 && $_REQUEST['TEXT']=='Approved'){	
+	// 2
+	if($istest2){
+		if($_REQUEST['TRTYPE'] == 0 &&  $_REQUEST['TEXT']=='Approved'){		 
+			$handler->payCompletion($_REQUEST, 24); //trtype = 24
+		} 
+	}
 
-		$order_id =  substr($_REQUEST['ORDER'], 5);
 
-		if ($order = $modx->getObject('msOrder', $order_id)) {  
-			$handler->receive($order, $_REQUEST);
+	// 3
+	if($istest3){
+
+		if($_REQUEST['TRTYPE'] == 0 &&  $_REQUEST['TEXT']=='Approved'){		 
+			$handler->payCompletion($_REQUEST, 21); //trtype = 21
+		} 
+
+		if($_REQUEST['TRTYPE'] == 21 &&  $_REQUEST['TEXT']=='Approved'){		 
+			$handler->payCompletion($_REQUEST, 24); //trtype = 24
+		} 
+
+	}
+
+
+	if(!$istest1 && !$istest2 && !$istest3){
+
+		// start real shop work 
+
+		if($_REQUEST['TRTYPE'] == 0 &&  $_REQUEST['TEXT']=='Approved'){		
+			$handler->payCompletion($_REQUEST);
 		}
-		else {
-			$modx->log(modX::LOG_LEVEL_ERROR, '[miniShop2:Victoriabankmd] Could not retrieve order with id '.$_REQUEST['ORDER']);
-		}
-	}	
+
+		if($_REQUEST['TRTYPE'] == 21 && $_REQUEST['TEXT']=='Approved'){	
+
+			$order_id =  substr($_REQUEST['ORDER'], 5);
+
+			if ($order = $modx->getObject('msOrder', $order_id)) {  
+				$handler->receive($order, $_REQUEST);
+			}
+			else {
+				$modx->log(modX::LOG_LEVEL_ERROR, '[miniShop2:Victoriabankmd] Could not retrieve order with id '.$_REQUEST['ORDER']);
+			}
+		}	
+	}
+ 
 }
